bb2b2b1c42cb168577fece8e9158bb325fabe1b6
RexShack <rexshackuk@gmail.com>
2025-08-10 12:11:05 +0100

    Initial commit

---
+++ installation/shared_items.lua
@@ -0,0 +1,7 @@
+    -----------------------------------------------
+    -- YOUR CUSTOM ITEMS
+    -----------------------------------------------
+    goldpan      = {name = 'goldpan',      label = 'Gold Pan',      weight = 100,  type = 'item', image = 'goldpan.png',      unique = false, useable = true,  shouldClose = true, description = 'add your description'},
+    smallnugget  = {name = 'smallnugget',  label = 'Small Nugget',  weight = 100,  type = 'item', image = 'smallnugget.png',  unique = false, useable = false, shouldClose = true, description = 'add your description'},
+    mediumnugget = {name = 'mediumnugget', label = 'Medium Nugget', weight = 100,  type = 'item', image = 'mediumnugget.png', unique = false, useable = false, shouldClose = true, description = 'add your description'},
+    largenugget  = {name = 'largenugget',  label = 'Large Nugget',  weight = 100,  type = 'item', image = 'largenugget.png',  unique = false, useable = false, shouldClose = true, description = 'add your description'},
