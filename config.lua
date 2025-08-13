c6323a94df0dfd0c6c64a615f06423700b9d48bf
Suu <takeshi.soufiane@gmail.com>
2025-08-13 11:01:42 +0200

    Update config.lua
    keep the same format as the original

--- config.lua
+++ config.lua
@@ -14,25 +14,25 @@ Config.LargeRewardAmount = 1 -- three rewards
 Config.GoldChance = 80 -- (80 = 20% changce of not finding gold) / (70 = 30% changce of not finding gold).. and so on
 
 -- Movement system
-Config.MinMovementDistance = 7.5 -- Distance minimum en mètres à parcourir
+Config.MinMovementDistance = 1.5 -- Distance minimum en mètres à parcourir
 Config.SpotMemoryTime = 300000 -- 5 minutes avant reset automatique
 
 Config.WaterTypes = {
-    [1] = {['name'] = 'San Luis River', ['waterhash'] = -1504425495, ['watertype'] = 'river'},
-    [2] = {['name'] = 'Upper Montana River', ['waterhash'] = -1781130443, ['watertype'] = 'river'},
-    [3] = {['name'] = 'Owanjila', ['waterhash'] = -1300497193, ['watertype'] = 'river'},
-    [4] = {['name'] = 'HawkEye Creek', ['waterhash'] = -1276586360, ['watertype'] = 'river'},
-    [5] = {['name'] = 'Little Creek River', ['waterhash'] = -1410384421, ['watertype'] = 'river'},
-    [6] = {['name'] = 'Dakota River', ['waterhash'] = 370072007, ['watertype'] = 'river'},
-    [7] = {['name'] = 'Beartooth Beck', ['waterhash'] = 650214731, ['watertype'] = 'river'},
-    [8] = {['name'] = 'Deadboot Creek', ['waterhash'] = 1245451421, ['watertype'] = 'river'},
-    [9] = {['name'] = 'Spider Gorge', ['waterhash'] = -218679770, ['watertype'] = 'river'},
-    [10] = {['name'] = 'Roanoke Valley', ['waterhash'] = -1229593481, ['watertype'] = 'river'},
-    [11] = {['name'] = 'Lannahechee River', ['waterhash'] = -2040708515, ['watertype'] = 'river'},
-    [12] = {['name'] = 'Random1', ['waterhash'] = 231313522, ['watertype'] = 'river'},
-    [13] = {['name'] = 'Random2', ['waterhash'] = 2005774838, ['watertype'] = 'river'},
-    [14] = {['name'] = 'Random3', ['waterhash'] = -1287619521, ['watertype'] = 'river'},
-    [15] = {['name'] = 'Random4', ['waterhash'] = -1308233316, ['watertype'] = 'river'},
-    [16] = {['name'] = 'Random5', ['waterhash'] = -196675805, ['watertype'] = 'river'},
-    [17] = {['name'] = 'Arroyo De La Vibora', ['waterhash'] = -49694339, ['watertype'] = 'river'},
+    [1] =  {['name'] = 'San Luis River',       ['waterhash'] = -1504425495, ['watertype'] = 'river'},
+    [2] =  {['name'] = 'Upper Montana River',  ['waterhash'] = -1781130443, ['watertype'] = 'river'},
+    [3] =  {['name'] = 'Owanjila',             ['waterhash'] = -1300497193, ['watertype'] = 'river'},
+    [4] =  {['name'] = 'HawkEye Creek',        ['waterhash'] = -1276586360, ['watertype'] = 'river'},
+    [5] =  {['name'] = 'Little Creek River',   ['waterhash'] = -1410384421, ['watertype'] = 'river'},
+    [6] =  {['name'] = 'Dakota River',         ['waterhash'] = 370072007,   ['watertype'] = 'river'},
+    [7] =  {['name'] = 'Beartooth Beck',       ['waterhash'] = 650214731,   ['watertype'] = 'river'},
+    [8] =  {['name'] = 'Deadboot Creek',       ['waterhash'] = 1245451421,  ['watertype'] = 'river'},
+    [9] =  {['name'] = 'Spider Gorge',         ['waterhash'] = -218679770,  ['watertype'] = 'river'},
+    [10] =  {['name'] = 'Roanoke Valley',      ['waterhash'] = -1229593481, ['watertype'] = 'river'},
+    [11] =  {['name'] = 'Lannahechee River',   ['waterhash'] = -2040708515, ['watertype'] = 'river'},
+    [12] =  {['name'] = 'Random1',             ['waterhash'] = 231313522,   ['watertype'] = 'river'},
+    [13] =  {['name'] = 'Random2',             ['waterhash'] = 2005774838,  ['watertype'] = 'river'},
+    [14] =  {['name'] = 'Random3',             ['waterhash'] = -1287619521, ['watertype'] = 'river'},
+    [15] =  {['name'] = 'Random4',             ['waterhash'] = -1308233316, ['watertype'] = 'river'},
+    [16] =  {['name'] = 'Random5',             ['waterhash'] = -196675805,  ['watertype'] = 'river'},
+    [17] =  {['name'] = 'Arroyo De La Vibora', ['waterhash'] = -49694339,   ['watertype'] = 'river'},
 }
